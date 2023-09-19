296007795a67a34b64efae49e4af43849e3eb288
yu-so-young2 <sy980911@gmail.com>
2023-09-19 19:05:33 +0900

    counter 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,19 @@
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Counter from "./pages/Counter";
 import {Routes, Route, Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/counter">Counter</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
+        <Route path="/counter" element={<Counter/>}></Route>
+
       </Routes>
     
     </div>
